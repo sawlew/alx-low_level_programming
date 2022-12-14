80de5456b1d76a3b1199b3473bc2370bd1d72c97
Soliu <sawlew@gmail.com>
2022-12-14 15:20:22 +0100

    update betty doc

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _islower - checks if letter c is lowercase.
+ * _islower - checks if a letter is lowercase.
+ * @c: The character to be checked.
  *
  * Return: 1 if the check is true, 0 therwise.
  */
