3c92ac517436d982bfa3e3b9536bcb18b9a9908e
Soliu <sawlew@gmail.com>
2022-12-15 22:47:20 +0100

    Num

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,18 +6,14 @@
  */
 void more_numbers(void)
 {
-int i, j;
+	char i, j;
 
-for (i = 0; i < 10; i++)
-{
-for (j  = 0; j <= 14, j++)
-{
-if (j >= 9)
-{
-_putchar((j / 10) + '0');
-}
-_putchar((j % 10) + '0');
-}
-_putchar('\n');
-}
+	while (i <= '9')
+	{
+		for (j  = '0'; j <= '14', j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
 }
