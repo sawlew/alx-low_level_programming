1a4876cf6a80e2e5decad02d7f9e6b3f12f3bb09
Soliu <sawlew@gmail.com>
2022-12-16 00:39:27 +0100

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,16 +10,19 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	if (size > 0)
-		for (i = size; i > 0; i--)
-		{
-			for (j = 1; j <= size; j++)
-				if (j >= i)
-					_putchar('#');
-				else
-					_printchar(' ');
-			_putchar('\n');
-		}
-	else
+	if (size <= 0)
 		_putchar('\n');
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
+		{
+				if (j <= (size - i))
+					_putchar(' ');
+				else
+					_printchar('#');
+
+		}
+		_putchar('\n');
+	}
+
 }
