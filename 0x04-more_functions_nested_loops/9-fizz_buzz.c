5fbee57cd4a09a43671fa7e8c4d8012ae9970672
Soliu <sawlew@gmail.com>
2022-12-16 00:19:35 +0100

    Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,7 +18,7 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i == 100)
-			printf("%s ", b);
+			printf("%s", b);
 		else if ((i % 3 == 0) && (i % 5 == 0))
 			printf("%s ", fb);
 		else if (i % 3 == 0)
