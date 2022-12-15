b5aa7e5150dd2a92e7e4db1ae2a259203e34e351
Soliu <sawlew@gmail.com>
2022-12-15 18:12:18 +0100

    upper lower checker

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * main - header file for prototypes
- * void prototypes (void)
- * int prototypes (int)
- */
-int _putchar(char);
+
+int _putchar(char c);
 int _isupper(int c);
 
 #endif /* MAIN_H */
