33154117c40cfa575375ef6598f57546d75c5f71
Soliu <sawlew@gmail.com>
2022-12-16 08:03:40 +0100

    Triad

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,19 +10,19 @@ void print_triangle(int size)
 {
 int i, j;
 
-if (size > 0)
+if (size <= 0)
+_putchar('\n');
 
-for (i = size; i > 0; i++)
+for (i = 1; i <= size; i++)
 {
 for (j = 1; j <= size; j++)
 {
-if (j >= i)
-_putchar('#');
+if (j <= (size - i))
+_putchar(' ');
 else
-_printchar(' ');
+_printchar('#');
+}
 
 _putchar('\n');
 }
-else
-_putchar('\n');
 }
