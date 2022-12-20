1a44c0a8730a85fb74375b4641a74650b152654f
Soliu <sawlew@gmail.com>
2022-12-20 01:42:01 +0100

    hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,44 +10,44 @@
 
 int main(void)
 {
-	char pass[84];
-	int index = 0, sum = 0, a, b;
+char pass[84];
+int index = 0, sum = 0, a, b;
 
-	srand(time(0));
+srand(time(0));
 
-	while (sum< 2772)
-	{
-		pass[index] = 33 + rand() % 94;
-		sum += pass[index++];
-	}
-	pass[index] = '\0';
+while (sum< 2772)
+{
+pass[index] = 33 + rand() % 94;
+sum += pass[index++];
+}
+pass[index] = '\0';
 
-	if (sum != 2772)
-	{
-		a = (sum - 2772) / 2;
-		b = (sum - 2772) / 2;
-		if ((sum - 2772) % 2 != 0)
-			a++;
+if (sum != 2772)
+{
+a = (sum - 2772) / 2;
+b = (sum - 2772) / 2;
+if ((sum - 2772) % 2 != 0)
+a++;
 
-			for (index = 0; pass[index]; index++)
-			{
-				if (pass[index] >= (33 + a))
-				{
-					pass[index] -= a;
-					break;
-				}
-			}
-			for (index = 0; pass[index]; index++)
-			{
-				if (pass[index] >= (33 + b))
-				{
-					pass[index] -= b;
-					break;
-				}
-			}
-	}
+for (index = 0; pass[index]; index++)
+{
+if (pass[index] >= (33 + a))
+{
+pass[index] -= a;
+break;
+}
+}
+for (index = 0; pass[index]; index++)
+{
+if (pass[index] >= (33 + b))
+{
+pass[index] -= b;
+break;
+}
+}
+}
 
-	printf("%s", pass);
+printf("%s", pass);
 
-	return (0);
+return (0);
 }
