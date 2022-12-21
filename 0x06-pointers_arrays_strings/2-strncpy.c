2034ba63e4c094e7ac1abcabfcfa901785545fc3
Soliu <sawlew@gmail.com>
2022-12-21 19:56:47 +0100

    string copy edit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,15 +9,15 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int a;
+int a;
 
-	for (a = 0; a < n && *(src + a); a++)
-	{
-		*(dest + a) = *(src + a);
-	}
-	for (a < n; a++)
-	{
-		*(dest + a) = '\0';
-	}
-	return (dest);
+for (a = 0; a < n && *(src + a); a++)
+{
+*(dest + a) = *(src + a);
+}
+for (a < n; a++)
+{
+*(dest + a) = '\0';
+}
+return (dest);
 }
