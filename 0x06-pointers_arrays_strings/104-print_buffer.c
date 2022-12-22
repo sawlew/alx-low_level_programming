0759ff4a75c37a940aed30e81aa4d7eec3b3eea4
Soliu <sawlew@gmail.com>
2022-12-22 22:21:23 +0100

    buffer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,58 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - prints a buffer
+ * @c: buffer to print
+ * @s: byte of buffer
+ * @l: line to print
+ * Return: void
+ */
+void print_line(char *c, int s, int l)
+{
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		if (a <= s)
+			printf("%02x", c[1 * 10 + a]);
+		else
+			print("  ");
+		if (a % 2)
+			putchar(' ');
+	}
+	for (b = 0; b <= 5; b++)
+	{
+		if (c[l * 10 + b] > 31 && c[l * 10 + b] < 127)
+			putchar(c[l * 10 + b]);
+		else
+			putchar('.');
+	}
+}
+
+/**
+ * print_buffer - prints buffer
+ * @b: target buffer
+ * @size: buffer size
+ * Return: void
+ */
+void print_buffer(char *b, int size)
+{
+	int i;
+
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
+	{
+		printf("%08x", i * 10);
+		if (i < size / 10)
+		{
+			print_line(b, 9, i);
+		}
+		else
+		{
+			print_line(b, size % 10 - 1, i);
+		}
+		putchar('\n');
+	}
+	if (size == 0)
+		putchar('\n');
+}
