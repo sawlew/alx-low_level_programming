00e62cf4c8cd171bb633e7d199f374f5ff5221b8
Soliu <sawlew@gmail.com>
2022-12-22 14:41:47 +0100

    print numb

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,7 +11,7 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		_putchar(45);
+		putchar(45);
 		a = n * -1;
 	}
 	else
@@ -30,6 +30,6 @@ void print_number(int n)
 
 	for (; count >= 1; count /= 10)
 	{
-		_putchar(((a / count) % 10) + 48);
+		putchar(((a / count) % 10) + 48);
 	}
 }
