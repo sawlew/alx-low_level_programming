e428c53e957e26f3f54283a7c4994b93dfd07d9c
Soliu <sawlew@gmail.com>
2023-01-03 10:28:46 +0100

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,10 +2,8 @@
 
 /**
  * _strstr - locates a substring
- *
  * @haystack: the longer string to search
  * @needle: the first occurrence of the substring
- *
  * Return: a pointer beg of substring or @Null if it not foound.
  */
 char *_strstr(char *haystack, char *needle)
