71ba03ff412b34612391a2b983d6bbd0c47f0f18
Soliu <sawlew@gmail.com>
2023-01-04 09:24:07 +0100

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,7 +7,9 @@
 int _strlen_recursion(char *s)
 {
 	if (s[0] != '\0')
+	{
 		return (1 + _strlen_recursion(s + 1));
+	}
 	return (0);
 }
 /**
@@ -20,12 +22,20 @@ int _strlen_recursion(char *s)
 int pal_checker(char *s, int i, int j)
 {
 	if (s[i] == s[j])
+	{
 		if (i > j / 2)
+		{
 			return (1);
+		}
 		else
+		{
 			return (pal_checker(s, i + 1, j - 1));
+		}
+	}
 	else
+	{
 		return (0);
+	}
 }
 /**
  * is_palindrome - check if s is palindrome
