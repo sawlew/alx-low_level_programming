a9d641117abeeab8cb2f26bdbdab8ba6c4b9d5e2
Soliu <sawlew@gmail.com>
2023-01-04 08:57:33 +0100

    poer

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _pow_recursion - power
+ * _pow_recursion - evaluates the power of the given numbers
  * @x:int
  * @y:int
  * Return:int
@@ -13,8 +13,9 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	}
 	else if (y != 0)
+	{
 		return (x * _pow_recursion(x, y - 1));
-
+	}
 	else
 	{
 		return (1);
