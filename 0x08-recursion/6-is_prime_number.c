11b72ac294356ee28889f2890f815b55a475273c
Soliu <sawlew@gmail.com>
2023-01-04 09:14:05 +0100

    prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,11 +9,17 @@
 int check(int a, int b)
 {
 	if (b < 2 || b % a == 0)
+	{
 		return (0);
+	}
 	else if (a > b / 2)
+	{
 		return (1);
+	}
 	else
+	{
 		return (check(a + 1, b));
+	}
 }
 
 /**
@@ -24,6 +30,8 @@ int check(int a, int b)
 int is_prime_number(int n)
 {
 	if (n == 2)
+	{
 		return (1);
+	}
 	return (check(2, n));
 }
