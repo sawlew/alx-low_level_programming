cda87ad7e56d5c69d410a2556537d672bf550583
Soliu <sawlew@gmail.com>
2023-01-07 05:35:26 +0100

    chan

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -5,7 +5,9 @@
  * main - prints the minimum number of coins to make change for an amount of money
  * @argc: argument count
  * @argv: arguments
- * Return: return 1 if the number of argument is one and 0 otherwise
+ *
+ * Return: return - 1
+ * 	Otherwise - 0
  */
 
 int main(int argc, char *argv[])
