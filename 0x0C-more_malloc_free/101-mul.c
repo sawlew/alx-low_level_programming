7f7ae169a55ee63dea77ce2594fa261184aacc08
Soliu Adeosun <sawlew@gmail.com>
2023-01-12 15:57:34 +0100

    adj

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -22,28 +22,8 @@ int i, j;
 			exit(98); }
 		}
 
-<<<<<<< HEAD
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	for (num1 = 1; num1 < argc; num1++)
-	{
-		for (num2 = 0; argv[num1][num2] != '\0'; num2++)
-		{
-			if (argv[num1][num2] > 57 || argv[num1][num2] < 48)
-			{
-				printf("Error\n");
-				exit(98);
-			}
-		}
-	}
-	mul = atol(argv[1]) *atol(argv[2]);
-=======
 	}
 	mul = atol(argv[1]) *  atol(argv[2]);
->>>>>>> 000d716b152505d79596b8fa0afa58ec4f74dcb6
 	printf("%lu\n", mul);
 return (0);
-}
+    }
