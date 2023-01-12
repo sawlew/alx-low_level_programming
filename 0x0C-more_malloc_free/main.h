77b415977c8f3386a03638ebb2e5566824d8b398
Soliu <sawlew@gmail.com>
2023-01-12 15:01:02 +0100

    mutiply

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -13,5 +13,6 @@ void _print(char *str, int l);
 char *mul(char n, char *num, int num_index, char *dest, int dest_index);
 int check_for_digits(char **av);
 void init(char *str, int l);
+int main(int argc, char *argv[]);
 
 #endif/*MAIN_H*/
