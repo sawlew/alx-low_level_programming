3377948e10c8f2478b63316145ab10cf8d04bb7c
Soliu <sawlew@gmail.com>
2023-01-12 12:36:52 +0100

    malloc_checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,9 +2,7 @@
 
 /**
  * malloc_checked - cause normal process termination with a status value of 98
- *
  * @b: allocated memory
- *
  * Return: a pointer to the allocated memory
  */
 void *malloc_checked(unsigned int b)
@@ -13,7 +11,8 @@ void *malloc_checked(unsigned int b)
 
 	i = malloc(b);
 	if (i == NULL)
+	{
 		exit(98);
+	}
 	return (i);
-
 }
