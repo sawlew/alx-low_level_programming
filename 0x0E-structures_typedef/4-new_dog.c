f1657ed6b189f2d2e7ed5e9d243d869a38279a2d
Soliu <sawlew@gmail.com>
2023-01-17 00:15:52 +0100

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,9 +6,9 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
- *
  * Return: pointer to new dog
  */
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	unsigned int nl, ol, i;
