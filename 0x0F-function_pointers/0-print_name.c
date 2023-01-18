66ac85642905988f046184823990a1d14f2b3560
Soliu <sawlew@gmail.com>
2023-01-18 14:42:38 +0100

    task 1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,9 +4,9 @@
  * print_name - a function that prints a name.
  * @name: the name given
  * @f: function of name
- *
  * Rteurn: void
  */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
