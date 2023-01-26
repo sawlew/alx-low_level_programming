a48c55a6b3de368b7f0ebbd52e9108ab7ff2e6b8
Soliu <sawlew@gmail.com>
2023-01-26 16:22:54 +0100

    test

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -15,4 +15,5 @@ void myStartupFun(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
 }
